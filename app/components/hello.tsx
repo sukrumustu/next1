f0133dfd28b81ea520d001e47ec2f54865d54f1f
sukrumustu <smustu1981@gmail.com>
2025-02-24 13:40:21 +0300

    next1 commit-2

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+function Hello () {
+    console.log("Hello, I'm a client-side component");
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+
+}
+
+export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-
+import Hello from "./components/hello";
 export default function Home() {
 
   console.log('What am I doing here --- SERVER / ClIENT') // Bu sayda server-side olması nedeniyle normalde sadece terminalde görünmesi gerekir, ancak artık tarayıcı konsolunda da görünüyor . SERVER olduğu yazıyor ...
 
   return (
+
+    <>
     <h1 className="text-3xl">Welcome NextJS</h1>
+    <Hello />
+    </>
+    
   );
 }
